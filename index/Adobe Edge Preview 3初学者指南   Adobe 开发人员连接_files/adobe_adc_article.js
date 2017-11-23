fb4fec2f0774a1d3d2b7a04aeefb1529a2b27650
QuietClickCode <2572417548@qq.com>
2017-11-23 11:37:52 +0800

    xxx

--- index/Adobe Edge Preview 3初学者指南   Adobe 开发人员连接_files/adobe_adc_article.js
+++
@@ -1,7 +0,0 @@
-
-                var dispatcher = adobe.vrbl("reflowDispatcher", adobe.reflow.createDispatcher("width", document));
-                
-            dispatcher.addLayoutEvent(new adobe.reflow.LayoutEvent("tablet", 0, 960, true));
-        
-            dispatcher.addLayoutEvent(new adobe.reflow.LayoutEvent("phone", 0, 359, true));
-        
